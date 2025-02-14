6169a1ed650ba2197b2e15c21e1d7f6456b276cf
Basile <basile.pineau64@orange.fr>
2025-02-14 15:25:55 +0100

    refactor(symfony): migrate from Symfony 4.4 to Symfony 5.4

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
@@ -10,5 +9,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true], 
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
