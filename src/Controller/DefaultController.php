057fb0a374ef02e0e83df21dd7d5d1e158fa101c
Basile <basile.pineau64@orange.fr>
2025-02-17 10:50:58 +0100

    refactor(symfony): migrate from Symfony 5.4 to Symfony 6.4 but still issues to fix

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,14 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction()
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
     {
         return $this->render('default/index.html.twig');
     }
