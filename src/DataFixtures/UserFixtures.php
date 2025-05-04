01530e7540326cf8e6288c1fde86d94f09044590
Basile <basile.pineau64@orange.fr>
2025-05-04 15:13:33 +0200

    feat(fixtures): create User and Task fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Enum\RoleEnum;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public const USER_REFERENCE = 'user-';
+    public const ADMIN_REFERENCE = 'admin-';
+    public const ANONYMOUS_REFERENCE = 'anonymous-user';
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Utilisateur anonyme
+        $anonymous = new User();
+        $anonymous->setUsername('anonyme');
+        $anonymous->setEmail('anonyme@example.com');
+        $anonymous->setRoles([RoleEnum::ANONYMOUS]);
+        $anonymous->setPassword($this->hasher->hashPassword($anonymous, 'anonyme'));
+        $manager->persist($anonymous);
+        $this->addReference(self::ANONYMOUS_REFERENCE, $anonymous);
+
+        // Administrateur connu pour se connecter
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles([RoleEnum::ADMIN]);
+        $admin->setPassword($this->hasher->hashPassword($admin, 'admin1234'));
+        $manager->persist($admin);
+        $this->addReference(self::ADMIN_REFERENCE . 'main', $admin);
+
+        // 3 utilisateurs normaux
+        for ($i = 1; $i <= 3; $i++) {
+            $user = new User();
+            $user->setUsername("user$i");
+            $user->setEmail("user$i@example.com");
+            $user->setRoles([RoleEnum::USER]);
+            $user->setPassword($this->hasher->hashPassword($user, 'password'));
+            $manager->persist($user);
+            $this->addReference(self::USER_REFERENCE.$i, $user);
+        }
+
+        // 3 administrateurs
+        for ($i = 1; $i <= 3; $i++) {
+            $admin = new User();
+            $admin->setUsername("admin$i");
+            $admin->setEmail("admin$i@example.com");
+            $admin->setRoles([RoleEnum::ADMIN]);
+            $admin->setPassword($this->hasher->hashPassword($admin, 'adminpass'));
+            $manager->persist($admin);
+            $this->addReference(self::ADMIN_REFERENCE.$i, $admin);
+        }
+
+        $manager->flush();
+    }
+}
