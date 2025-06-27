24e616fb0da3c0e63b82ca05a41bf6870c653a1a
Basile <basile.pineau64@orange.fr>
2025-06-27 11:06:56 +0200

    test(controller): improve functional tests for TaskController

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -21,23 +21,21 @@ class TaskControllerTest extends WebTestCase
         $this->taskRepository = $this->em->getRepository(Task::class);
         $this->userRepository = $this->em->getRepository(User::class);
         $this->cleanTestUsers();
-        $this->ensureTestUserExists();
+        $this->createTestUsers();
     }
 
     public function testTaskList(): void
     {
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
+        $this->loginUserByEmail('test@user.com');
 
-        $crawler = $this->client->request('GET', '/tasks');
+        $this->client->request('GET', '/tasks');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('a.btn.btn-info', 'Créer une tâche');
     }
 
     public function testTaskCreate(): void
     {
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
+        $this->loginUserByEmail('test@user.com');
 
         $crawler = $this->client->request('GET', '/tasks/create');
         $form = $crawler->selectButton('Ajouter')->form([
@@ -51,136 +49,15 @@ class TaskControllerTest extends WebTestCase
         $this->assertSelectorTextContains('.alert-success', 'La tâche a été bien été ajoutée.');
     }
 
-    public function testToggleTask(): void
-    {
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
-
-        $task = new Task();
-        $task->setTitle('Toggle task');
-        $task->setContent('Test toggle');
-        $task->setUser($user);
-        $task->setIsDone(false);
-        $this->em->persist($task);
-        $this->em->flush();
-
-        $this->client->request('GET', '/tasks/' . $task->getId() . '/toggle');
-        $this->assertResponseRedirects('/tasks');
-    }
-
-    public function testDeleteTask(): void
-    {
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
-
-        $task = new Task();
-        $task->setTitle('Delete task');
-        $task->setContent('Test delete');
-        $task->setUser($user);
-        $this->em->persist($task);
-        $this->em->flush();
-
-        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
-        $this->assertResponseRedirects('/tasks');
-        $this->client->followRedirect();
-        $this->assertSelectorTextContains('.alert-success', 'La tâche a bien été supprimée.');
-    }
-
-    public function testCannotDeleteTaskIfNotOwner(): void
-    {
-        $owner = new User();
-        $owner->setUsername('Owner user');
-        $owner->setEmail('owner@user.com');
-        $owner->setPassword('password');
-        $owner->setRoles([RoleEnum::USER]);
-        $this->em->persist($owner);
-
-        $task = new Task();
-        $task->setTitle('Not my task');
-        $task->setContent('Should not delete');
-        $task->setUser($owner);
-        $this->em->persist($task);
-
-        $this->em->flush();
-
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
-
-        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
-        $this->assertResponseRedirects('/tasks');
-        $this->client->followRedirect();
-        $this->assertSelectorExists('.alert');
-    }
-
-    public function testAdminCanDeleteAnonymousTask(): void
-    {
-        $anonymous = new User();
-        $anonymous->setUsername('Anonymous user');
-        $anonymous->setEmail('anon@anon.com');
-        $anonymous->setPassword('password');
-        $anonymous->setRoles([RoleEnum::ANONYMOUS]);
-        $this->em->persist($anonymous);
-
-        $task = new Task();
-        $task->setTitle('Anonymous task');
-        $task->setContent('Admin should delete');
-        $task->setUser($anonymous);
-        $this->em->persist($task);
-
-        $admin = new User();
-        $admin->setUsername('Admin user');
-        $admin->setEmail('admin@user.com');
-        $admin->setPassword('password');
-        $admin->setRoles([RoleEnum::ADMIN]);
-        $this->em->persist($admin);
-
-        $this->em->flush();
-
-        $this->client->loginUser($admin);
-
-        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
-        $this->assertResponseRedirects('/tasks');
-        $this->client->followRedirect();
-        $this->assertSelectorTextContains('.alert-success', 'La tâche a bien été supprimée.');
-    }
-
-    public function testCannotDeleteAnonymousTaskIfNotAdmin(): void
-    {
-        $anon = new User();
-        $anon->setUsername('Anon');
-        $anon->setEmail('anon@anon.com');
-        $anon->setPassword('password');
-        $anon->setRoles([RoleEnum::ANONYMOUS]);
-        $this->em->persist($anon);
-
-        $task = new Task();
-        $task->setTitle('Task anonymous');
-        $task->setContent('Should fail delete');
-        $task->setUser($anon);
-        $this->em->persist($task);
-
-        $this->em->flush();
-
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
-
-        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
-        $this->assertResponseRedirects('/tasks');
-        $this->client->followRedirect();
-        $this->assertSelectorTextContains('.alert', 'Seuls les administrateurs peuvent supprimer une tâche');
-    }
-
     public function testEditTaskAllowedForOwner(): void
     {
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
+        $this->loginUserByEmail('test@user.com');
 
-        $task = new Task();
-        $task->setTitle('Edit me');
-        $task->setContent('Before edit');
-        $task->setUser($user);
-        $this->em->persist($task);
-        $this->em->flush();
+        $task = $this->createTaskForUser(
+            $this->userRepository->findOneBy(['email' => 'test@user.com']),
+            'Edit me',
+            'Before edit'
+        );
 
         $crawler = $this->client->request('GET', '/tasks/' . $task->getId() . '/edit');
 
@@ -197,22 +74,15 @@ class TaskControllerTest extends WebTestCase
 
     public function testEditTaskDeniedForNonOwner(): void
     {
-        $owner = new User();
-        $owner->setUsername('Not me');
-        $owner->setEmail('notme@user.com');
-        $owner->setPassword('password');
-        $owner->setRoles([RoleEnum::USER]);
-        $this->em->persist($owner);
+        $owner = $this->userRepository->findOneBy(['email' => 'notme@user.com']);
 
-        $task = new Task();
-        $task->setTitle('Blocked edit');
-        $task->setContent('You shall not edit');
-        $task->setUser($owner);
-        $this->em->persist($task);
-        $this->em->flush();
+        $task = $this->createTaskForUser(
+            $owner,
+            'Blocked edit',
+            'You shall not edit'
+        );
 
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
-        $this->client->loginUser($user);
+        $this->loginUserByEmail('test@user.com');
 
         $this->client->request('GET', '/tasks/' . $task->getId() . '/edit');
         $this->assertResponseRedirects();
@@ -220,19 +90,122 @@ class TaskControllerTest extends WebTestCase
         $this->assertSelectorExists('.alert');
     }
 
-    private function ensureTestUserExists(): void
+    public function testToggleTask(): void
     {
-        $user = $this->userRepository->findOneBy(['email' => 'test@user.com']);
+        $this->loginUserByEmail('test@user.com');
 
-        if (!$user) {
-            $user = new User();
-            $user->setUsername('Test user');
-            $user->setEmail('test@user.com');
-            $user->setPassword('password');
-            $user->setRoles([RoleEnum::USER]);
-            $this->em->persist($user);
-            $this->em->flush();
+        $task = $this->createTaskForUser(
+            $this->userRepository->findOneBy(['email' => 'test@user.com']),
+            'Toggle task',
+            'Test toggle',
+            false
+        );
+
+        $this->client->request('GET', '/tasks/' . $task->getId() . '/toggle');
+        $this->assertResponseRedirects('/tasks');
+    }
+
+    public function testDeleteTask(): void
+    {
+        $this->loginUserByEmail('test@user.com');
+
+        $task = $this->createTaskForUser(
+            $this->userRepository->findOneBy(['email' => 'test@user.com']),
+            'Delete task',
+            'Test delete'
+        );
+
+        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('.alert-success', 'La tâche a bien été supprimée.');
+    }
+
+    public function testCannotDeleteTaskIfNotOwner(): void
+    {
+        $owner = $this->userRepository->findOneBy(['email' => 'owner@user.com']);
+
+        $task = $this->createTaskForUser(
+            $owner,
+            'Not my task',
+            'Should not delete'
+        );
+
+        $this->loginUserByEmail('test@user.com');
+
+        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorExists('.alert');
+    }
+
+    public function testAdminCanDeleteAnonymousTask(): void
+    {
+        $anonymous = $this->userRepository->findOneBy(['email' => 'anon@anon.com']);
+        $admin = $this->userRepository->findOneBy(['email' => 'admin@user.com']);
+
+        $task = $this->createTaskForUser(
+            $anonymous,
+            'Anonymous task',
+            'Admin should delete'
+        );
+
+        $this->loginUserByEmail('admin@user.com');
+
+        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('.alert-success', 'La tâche a bien été supprimée.');
+    }
+
+    public function testCannotDeleteAnonymousTaskIfNotAdmin(): void
+    {
+        $anon = $this->userRepository->findOneBy(['email' => 'anon@anon.com']);
+
+        $task = $this->createTaskForUser(
+            $anon,
+            'Task anonymous',
+            'Should fail delete'
+        );
+
+        $this->loginUserByEmail('test@user.com');
+
+        $this->client->request('GET', '/tasks/' . $task->getId() . '/delete');
+        $this->assertResponseRedirects('/tasks');
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('.alert', 'Seuls les administrateurs peuvent supprimer une tâche');
+    }
+
+    private function loginUserByEmail(string $email): User
+    {
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+        $this->client->loginUser($user);
+        return $user;
+    }
+
+    private function createTestUsers(): void
+    {
+        $usersData = [
+            ['username' => 'Test user', 'email' => 'test@user.com', 'role' => RoleEnum::USER],
+            ['username' => 'Owner user', 'email' => 'owner@user.com', 'role' => RoleEnum::USER],
+            ['username' => 'Admin user', 'email' => 'admin@user.com', 'role' => RoleEnum::ADMIN],
+            ['username' => 'Anonymous user', 'email' => 'anon@anon.com', 'role' => RoleEnum::ANONYMOUS],
+            ['username' => 'Not me', 'email' => 'notme@user.com', 'role' => RoleEnum::USER],
+        ];
+
+        foreach ($usersData as $userData) {
+            $existingUser = $this->userRepository->findOneBy(['email' => $userData['email']]);
+            if (!$existingUser) {
+                $user = new User();
+                $user->setUsername($userData['username']);
+                $user->setEmail($userData['email']);
+                $user->setPassword('password');
+                $user->setRoles([$userData['role']]);
+                $this->em->persist($user);
+            }
         }
+
+        $this->em->flush();
     }
 
     private function cleanTestUsers(): void
@@ -248,5 +221,19 @@ class TaskControllerTest extends WebTestCase
             }
         }
         $this->em->flush();
+    }
+
+    private function createTaskForUser(User $user, string $title, string $content = '', bool $isDone = false): Task
+    {
+        $task = new Task();
+        $task->setTitle($title);
+        $task->setContent($content);
+        $task->setUser($user);
+        $task->setIsDone($isDone);
+
+        $this->em->persist($task);
+        $this->em->flush();
+
+        return $task;
     }
 }
